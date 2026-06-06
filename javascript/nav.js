576a250cd67cc9ecfe71ee42171d45e25b1362af
izorget <izorgetprods@gmail.com>
2026-06-06 20:34:29 +0200

    Font Change and Home page layout

---
+++ javascript/nav.js
@@ -0,0 +1,25 @@
+document.getElementById('nav-container').innerHTML = 
+`
+    <nav class="main-nav">
+        <div class="nav-container">
+            <div class="nav-portfolio-name">
+                <a href="index.html" class="logo">Unathi Shibe</a>
+            </div>
+            <button class="mobile-menu-btn" id="mobileMenuBtn">☰</button>
+            <ul class="nav-links" id="navLinks">
+                <li><a href="index.html" class="nav-link">Home</a></li>
+                <li><a href="pages/3d-art.html" class="nav-link">3D Art</a></li>
+                <li><a href="pages/animation-unity.html" class="nav-link">Animation & Unity</a></li>
+                <li><a href="pages/about.html" class="nav-link">About</a></li>
+            </ul>
+        </div>
+    </nav>
+`;
+
+
+const currentPage = window.location.pathname.split('/').pop() || 'index.html';
+document.querySelectorAll('.nav-link').forEach(link => {
+    if (link.getAttribute('href').includes(currentPage)) {
+        link.classList.add('active');
+    }
+});
